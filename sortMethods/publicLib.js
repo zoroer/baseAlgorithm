215ab8b4f37a2c91a2e570ade14e6d5b4602ea6f
zoroer <haihanh2@163.com>
2017-04-25 21:21:16 +0800

    add searchMethods

---
+++ sortMethods/publicLib.js
@@ -0,0 +1,16 @@
+/**
+ * Created by CloudScope on 2017/4/19.
+ */
+
+/**
+ * 随机产生长度为length的数组并返回
+ * @param length
+ * @returns {Array}
+ */
+function createData(length) {
+    let arr = [];
+    for (let i = 0; i < length; i++) {
+        arr.push(parseInt(Math.random() * 50));
+    }
+    return arr;
+}
